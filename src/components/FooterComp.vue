a78567f2c8f848fa1f439c19b37b911c1e7ae23a
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-05-31 17:21:20 +0200

    Liste statiche footer.

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <section class="container">
+    <section class="container content">
       <div class="left-half">
         <div class="footer-links">
           <h4>Address</h4>
@@ -11,7 +11,33 @@
           </ul>
         </div>
       </div>
-      <div class="right-half"></div>
+      <div class="right-half">
+        <div class="footer-links">
+          <h4>Explore</h4>
+          <ul>
+            <li><a href="">Start here</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">About us</a></li>
+          </ul>
+        </div>
+        <div class="footer-links">
+          <h4></h4>
+          <ul>
+            <li><a href="">Success story</a></li>
+            <li><a href="">Courses</a></li>
+            <li><a href="">Contact us</a></li>
+          </ul>
+        </div>
+        <div class="footer-links">
+          <h4>Information</h4>
+          <ul>
+            <li><a href="">Membership</a></li>
+            <li><a href="">Purchase guide</a></li>
+            <li><a href="">Privacy policy</a></li>
+            <li><a href="">Terms of service</a></li>
+          </ul>
+        </div>
+      </div>
     </section>
   </footer>
 </template>
@@ -24,29 +50,33 @@ export default {
 
 <style lang="scss" scoped>
 footer{
-  display: flex;
-  min-height: 405px;
   background-color: #171520;
-  padding-top: 68px;
-  .left-half,
-  .right-half{
-    width: 50%;
-  }
-  .footer-links{
-    h4{
-      color: white;
-      font-size: 17px;
-      font-weight: bold;
-      margin-bottom: 17px;
+  .content{
+    display: flex;
+    min-height: 405px;
+    padding-top: 66px;
+    .left-half,
+    .right-half{
+      display: flex;
+      width: 50%;
     }
-    ul{
-      font-size: 14px;
-      li{
-        display: block;
-        margin: 14px 0 0px 0;
+    .footer-links{
+      margin-right: 67px;
+      h4{
+        color: white;
+        font-size: 17px;
+        font-weight: bold;
+        height: 24px;
       }
-      a{
-        color: #bbbcbd;
+      ul{
+        font-size: 14px;
+        li{
+          display: block;
+          margin: 14px 0 0px 0;
+        }
+        a{
+          color: #bbbcbd;
+        }
       }
     }
   }
